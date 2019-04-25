eea3bd2316b0a8a304c0a8828bab4851579d6f6f
hock toh <yhtthomas@gmail.com>
2019-04-25 13:25:33 +0800

    add image file type checking

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express')
 const multer  = require('multer')
+import {imageFilter} from './ImageFilter';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/Users/HockToh/TestPicture')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
-    }
-  })
-  
-  var upload = multer({ storage: storage })
-
-  const app = express()
-  const port = 3000
-
-app.post('/imageUploader', upload.single('image'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
+  destination: function (req, file, cb) {
+    cb(null, '/Users/HockToh/TestPicture')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+  }
 })
 
-// app.get('/', (req, res) => res.send('Hello Baby!'))
+var upload = multer({ storage: storage, fileFilter: imageFilter });
+
+const app = express()
+const port = 3000
+
+app.post('/imageUploader', upload.single('image'), function (err, req, res, next) {
+
+  if(err)
+  {
+    res.status(500).send(err.message);
+  } else {
+    res.status(200).send('I will give you a permanennt link to do upload');
+  }
+
+  // res.status(200).send('I will give you a permanennt link to do upload');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
